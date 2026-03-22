4696d6b9140339ea2fbd51916a4f88fa05de8381
MAX <onne628@proton.me>
2026-03-22 21:41:26 +0800

    修改vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+const isDev = process.env.NODE_ENV === "development";
+
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +13,13 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    isDev && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
